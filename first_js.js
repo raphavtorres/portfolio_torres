125f1a735ba8f7d92237b60cbbefc23a8c0cb593
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-04-14 11:26:15 -0300

    Add files via upload

---
+++ first_js.js
@@ -0,0 +1,16 @@
+function hello(){
+    window.alert('hello')
+}
+
+function sum(){
+    var n1 = parseInt(prompt("Write a number: "))
+    var n2 = parseInt(prompt("Write a second number: "))
+
+    /* var == global scope */
+    var sum = n1 + n2
+    alert(sum)
+}
+
+function pairOdd() {
+    pass
+}
